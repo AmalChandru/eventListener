231ec9ef7bfa9a9b6bceb177ed65e29a58e42588
AmalChandru <amaltathva@gmail.com>
2021-05-19 14:38:51 +0530

    Updated Feature: Add new Events

--- django_project/eventPortal/urls.py
+++ django_project/eventPortal/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='eventPortal-home'),
+    path('', PostListView.as_view(), name='eventPortal-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='eventPortal-about'),
 ]
--- django_project/eventPortal/views.py
+++ django_project/eventPortal/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
@@ -7,6 +8,19 @@ def home(request):
     'posts' : Post.objects.all()
     }
     return render(request, 'eventPortal/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'eventPortal/home.html'
+    context_object_name = 'posts'
+    ordering = ['date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
 
 def about(request):
     return render(request, 'eventPortal/about.html',{'title': 'About'})
