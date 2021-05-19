63598ec53be87ce6a8621fac52c392d66c42ca2a
AmalChandru <amaltathva@gmail.com>
2021-05-19 09:40:01 +0530

    Added login and logout facility

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators  import login_required
 from .forms import UserRegisterForm
 
 def register(request):
@@ -8,8 +9,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created. Welcome onBoard😍 {username}!')
-            return redirect('eventPortal-home')
+            messages.success(request, f'Account created. You are now able to login. Welcome onBoard😍 {username}!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
