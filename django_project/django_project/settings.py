6d2e259bdcc0f3c6dfd152be96d2b86aea6a172d
AmalChandru <amaltathva@gmail.com>
2021-05-22 11:18:54 +0530

    Added .gitignore

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.login_rlogin_requiredequired
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pbgjq4koahu&9(47mo4sp*4jbx1=$_3i2@^2%!je*wmqm^v75f'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
